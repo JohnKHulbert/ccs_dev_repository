4981ee735b535872598eaa4751ebea5e544a6a68
John Hulbert <john@chasingcometstudios.com>
2023-04-13 19:47:15 -0500

    Hulbert

---
+++ login.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Chasing Comet Studios</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" href="lib/bootstrap/bootstrap.min.css" />
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon_ccs.png" />
+		
+		
+		<style>	
+			.nomargin {
+				margin: 10px;
+			}
+		</style>
+
+	</head>
+
+	<body>
+
+		<div class="center">
+			<img class="small" src="img/ccslogo.png"/>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="jumbotron center">
+						<h1 class="display-4">Chasing Comet Studios</h1>
+						<p class="lead">
+							Chasing Comet Studios is a multimedia production company specializing in character animation and podcasts.
+						</p>
+						
+						<hr class="my-4" />
+						
+						<h3>Inspiring viewers to think and laugh</h3>
+						
+						<a class="redbackground redborder center btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div>
+				<br />
+				<div class="accordion" id="accordion_1_cartoons">
+					<div class="card">
+						<div class="card-header" id="headingOne">
+							<h2 class="mb-0">
+								<button class="btn btn-link btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+									Cartoons
+								</button>
+							</h2>
+						</div>
+
+						<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#Cartoons">
+							<div class="card-body">
+								<p class="services">
+									Below please find embeded examples of animation work by Chasing Comet Studios.
+								</p>
+							</div>
+						</div>		
+					</div>
+				</div>		
+			</div>
+
+			<div>
+				<div class="accordion" id="accordion_2_podcasts">
+					<div class="card">
+						<div class="card-header" id="headingOne">
+							<h2 class="mb-0">
+								<button class="btn btn-link btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+									Podcasts
+								</button>
+							</h2>
+						</div>
+
+						<div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#Podcasts">
+							<div class="card-body">
+								<p class="services">
+									Below please find podcasts hosted by the Chasing Comet Studios Network.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>		
+			</div>
+			
+			<div>
+				<div class="accordion" id="accordion_3_contact">
+					<div class="card">
+						<div class="card-header" id="other_services_header">
+							<h2 class="mb-0">
+								<button class="btn btn-light btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+									Contact
+								</button>
+							</h2>
+						</div>
+
+						<div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#Contact">
+							<div class="card-body">
+								<p class="services">
+									Please send all inquiries to john@chasingcometstudios.com or simply send a direct message to @ChasingCometStudios on Instagram, YouTube, Facebook, or TikTok.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>		
+			</div>
+		</div>
+
+		<div class="logo_style"></div>
+
+		<footer>
+
+			<p class="nomargin center">
+				Chasing Comet Studios is a multimedia production company specializing in character animation and podcasts. 
+				<br />
+				Be sure to follow @ChasingCometStudios on social media.
+				<br />
+			</p>
+
+			<div class="center"> 			
+				<img class="footer_logo" src="img/ccslogo.png"/>
+			</div>
+
+		</footer>
+
+	</body>
+	
+	<script src="lib/jquery.3.6.4.min.js"></script>
+	<script src="lib/bootstrap/bootstrap.min.js"></script>
+	<script src="lib/popper.min.js"></script>
+	<script src="js/scripts.js"></script>
+	
+</html>
