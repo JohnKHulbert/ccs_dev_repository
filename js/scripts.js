ea85110ad3ae4dbf696f186a7cacc1552a3649fe
John Hulbert <john@chasingcometstudios.com>
2023-04-18 20:39:11 -0500

    Hulbert

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $("#info_div").html('<a href="http://www.google.com">Follow Chasing Comet Studio
 
 $(document).on("click", "#loginbtn", function() {
 	
-	window.location.replace("login.php");
+	window.location.href = "login.php";
 	
 	alert("1");
 	
