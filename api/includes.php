5603f8ed3a6e51730ccad45e4d60e6fdd0fa653e
John Hulbert <john@chasingcometstudios.com>
2023-04-18 20:22:46 -0500

    Hulbert

--- api/includes.php
+++ api/includes.php
@@ -3,7 +3,7 @@
 	function login_nav_markup() {
 		
 	?>
-		<nav>
+		<nav style="height:50px;">
 	
 			<button id="loginbtn" class="btn btn-primary redbackground redborder">Login</button>
 	
@@ -16,9 +16,20 @@
 	function logout_nav_markup() {
 
 	?>
-		<nav>
+		<nav style="height:50px;">
 	
 			<button id="logoutbtn" class="btn btn-primary redbackground redborder">Logout</button>
+	
+		</nav>
+		
+	<?php
+
+	};
+	
+	function nav_markup() {
+
+	?>
+		<nav style="height:50px;">
 	
 		</nav>
 		
