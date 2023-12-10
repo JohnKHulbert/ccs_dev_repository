0ea92e9c97c43f41f53b65428e9e1664c18e1da1
Mike Swisher <mike@swishersolutions.com>
2023-12-09 22:01:32 -0700

    Swish

--- index.php
+++ index.php
@@ -32,7 +32,8 @@
 		<div>
 			
 			<div class="center">
-				<img src="img/ccslogo.png"/>
+				<!-- <img src="img/ccslogo.png"/> -->
+				<img src="img/sexy_john_hulbert.jpg"/>
 			</div>
 			
 		</div>
